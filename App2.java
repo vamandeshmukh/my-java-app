b6c3d352578dd9d62158dbdf3b3053b246fff110
Vaman Deshmukh <45888115+vamandeshmukh@users.noreply.github.com>
2020-09-26 10:45:19 +0530

    FIrst commit
    first commit

---
+++ App2.java
@@ -0,0 +1,12 @@
+package jenkins.demo.app;
+/**
+ * 
+ * @author Vaman Deshmukh 
+ *
+ */
+
+public class App2 {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
